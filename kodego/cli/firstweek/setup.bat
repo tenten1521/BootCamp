9fd258c7289b4c6aae9f27431b35c2c3a816480e
tenten1521 <araneta.justen1521@gmail.com>
2023-05-19 11:23:32 +0800

    posting folder in bootcamp

---
+++ kodego/cli/firstweek/setup.bat
@@ -0,0 +1,15 @@
+echo off
+
+title setup we folder
+cls
+echo porchop
+echo creating folder
+mkdir home 
+mkdir about
+echo creating index.html
+echo justen > index.html 
+cd home 
+echo justen araneta >> inde.html
+echo success!
+pause
+
